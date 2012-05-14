d9b0bc05df7474c896086e1bdb7d0b45e226796d
Enno Rehling <enno.rehling@gmail.com>
2012-05-13 23:58:41 -0700

    Copyright notice update. It's 2012.

--- critbit.h
+++ critbit.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010-2011, Enno Rehling <enno@eressea.de>
+Copyright (c) 2012, Enno Rehling <enno@eressea.de>
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
