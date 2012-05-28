bce997781d333ac8eb2efce23f3c7b8ebff28110
Enno Rehling <enno.rehling@gmail.com>
2012-05-28 05:39:35 +0000

    add a command line option to run searches multiple times

--- benchmark.c
+++ benchmark.c
@@ -8,42 +8,46 @@
 
 int count_cb(const void * match, const void * key, size_t keylen, void *result)
 {
-  int * ctr = (int*)result;
+  long * ctr = (long*)result;
   ++*ctr;
   return 0;
 }
 
 int main(int argc, char** argv) {
   clock_t start, now;
-  long n = 1000;
+  long multi=1, i, n = 1000;
   int a, verbose = 0;
   critbit_tree cb = { 0 };
   
   for (a=1;argc>a;++a) {
     if (strcmp(argv[a], "-v")==0) verbose = 1;
+    else if (strcmp(argv[a], "-x")==0) multi = strtolh(argv[++a], 10);
     else {
       n = strtolh(argv[a], 10);
       break;
     }
   }
   start = clock();
-  while (n--) {
+  for (i=0;i!=n;++i) {
     char str[20];
-    sprintf(str, "%ld", n);
+    sprintf(str, "%ld", i);
     cb_insert_str(&cb, str);
   }
   now = clock();
-  printf("init: %f\n", (float)(now-start)/CLOCKS_PER_SEC);
+  printf("init: %.2fs\n", (float)(now-start)/CLOCKS_PER_SEC);
 
   start = clock();
-  for (;a<argc;++a) {
+  for (++a;a<argc;++a) {
     const char * prefix = argv[a];
-    int o = 0;
-    cb_foreach(&cb, prefix, strlen(prefix), count_cb, &o);
-    if (verbose) printf("%s matches: %d\n", prefix, o);
+    long m;
+    for (m=0;m!=multi;++m) {
+      long o = 0;
+      cb_foreach(&cb, prefix, strlen(prefix), count_cb, &o);
+      if (m==0 && verbose) printf("%ld matches for prefix %s in %ld strings\n", o, prefix, n);
+    }
   }
   now = clock();
-  printf("find: %f\n", (float)(now-start)/CLOCKS_PER_SEC);
+  printf("find: %.2fs\n", (float)(now-start)/CLOCKS_PER_SEC);
 
   return 0;
 }
--- naive.c
+++ naive.c
@@ -9,10 +9,11 @@ int main(int argc, char** argv) {
   clock_t start, now;
   const char ** strings;
   int a = 1, verbose = 0;
-  long i, n = 1000;
+  long i, multi = 1, n = 1000;
 
   for (a=1;argc>a;++a) {
     if (strcmp(argv[a], "-v")==0) verbose = 1;
+    else if (strcmp(argv[a], "-x")==0) multi = strtolh(argv[++a], 10);
     else {
       n = strtolh(argv[a], 10);
       break;
@@ -26,19 +27,22 @@ int main(int argc, char** argv) {
     strings[i] = strdup(str);
   }
   now = clock();
-  printf("init: %f\n", (float)(now-start)/CLOCKS_PER_SEC);
+  printf("init: %.2fs\n", (float)(now-start)/CLOCKS_PER_SEC);
 
   start = clock();
-  for (;a<argc;++a) {
+  for (++a;a<argc;++a) {
     const char * prefix = argv[a];
-    int o = 0;
-    size_t len = strlen(prefix);
-    for(i=0;i!=n;++i) {
-      if (strncmp(prefix, strings[i], len)==0) ++o;
+    long m;
+    for (m=0;m!=multi;++m) {
+      long o = 0;
+      size_t len = strlen(prefix);
+      for(i=0;i!=n;++i) {
+        if (strncmp(prefix, strings[i], len)==0) ++o;
+      }
+      if (m==0 && verbose) printf("%ld matches for prefix %s in %ld strings\n", o, prefix, n);
     }
-    if (verbose) printf("%s matches: %d\n", prefix, o);
   }
   now = clock();
-  printf("find: %f\n", (float)(now-start)/CLOCKS_PER_SEC);
+  printf("find: %.2fs\n", (float)(now-start)/CLOCKS_PER_SEC);
   return 0;
 }
